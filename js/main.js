fe016a65c5d1de0d6dae14006ffc3e35b3da0062
aoha968 <aoha.kurobane168@gmail.com>
2021-10-11 00:38:58 +0900

    <全体> ・クラス名称を「__」に統一 <Scss> ・優先度でスタイルがおかしくなっていたため、全体的に修正 ・@mediaの記載位置を変更 <JQuery> ・ウィンドウリサイズ処理が微妙だったため修正 ・ナビゲーションメニューも微妙だたため。PC/モバイルで処理を分けるように変更

--- js/main.js
+++ js/main.js
@@ -1,92 +1,87 @@
-/* (PC・モバイル版共通)
- * ナビゲーションメニュー第2階層
---------------------------------- */
-$(function(){
-    $(".menu-item").mouseover(function(){
-        /* ナビゲーションメニューを開く */
-        $(this).children(".sub-menu").stop().slideDown(200);
-    });
-    $(".menu-item").mouseout(function(){
-        /* ナビゲーションメニューを閉める */
-        $(".sub-menu").stop().slideUp(50);  /* 50以下にすると閉めるときにちらつく */
-    });
-});
-
-/* slideToggleで記載した場合 -> メニューの開閉速度が同じになるので今回は使用しない */
-// $(function(){
-//     $(".menu-item").hover(function(){
-//         $(this).children(".sub-menu").stop().slideToggle(200);
-//     });
-// });
-
-/* (PC版)
- * ナビゲーションメニューを上部に固定
+/* (共通)
+ * windowサイズ変更処理
 --------------------------------- */
 $(window).resize(function(){
-    /* 現在のwindow幅を取得 */
-    var curWidth = $(window).width();
-    var desktopWidth = 1184;    /* PC版の横幅の境界値 */
-
-    if (desktopWidth <= curWidth) {
-        /* PC版の横幅だったら、モバイル版で付与していたopen/closeクラスを削除 */
-        $('.Nav_body').addClass("close").removeClass('close');
-        if($(".Nav_body").css("display")=="none"){
-            $(".Nav_body").css({
-                'display':  'block' 
-            });
-        }
-    }else{
-        /*  モバイル版の横幅 かつ ウィンドウサイズ変更時は一度メニューを閉じる
-            クリックの処理は開閉処理で実施 */
-        $(".Nav_btn a").removeClass("open").addClass("close");
-        $(".Nav_body").addClass("close").removeClass("open").slideUp("fast");
-        $(".Nav_body li.menu-item-has-children").removeClass("open").addClass("close");
-        $(".Nav ul.sub-menu").slideUp("fast").removeClass("open").addClass("close");
-        $(this).addClass("close").removeClass("open").html("<span>メニューを開く</span>");
-    }
+	var windowWidth=window.innerWidth;
+	var point=1200;
+	var timer=false;
+	if(timer!==false){
+		clearTimeout(timer);
+	}
+	timer=setTimeout(function(){
+		if(windowWidth<=point){
+			$("body").addClass("spn").removeClass("pc");
+			$(".Nav__body").addClass("close").removeClass("open").slideUp("fast");
+			$(".Nav__btn a").addClass("close").removeClass("open").html("<span>メニューを開く</span>");
+			$(".Nav__body li.menu-item-has-children").removeClass("open").addClass("close");
+			$(".Nav ul.sub-menu").slideUp("fast").removeClass("open").addClass("close");
+		}else{
+			$("body").addClass("pc").removeClass("spn");
+			$(".Nav__body").removeClass("open close").css("display","block");
+			$(".Nav__btn a").removeClass("open close").html("<span>メニューを閉じる</span>");
+			$(".Nav__body li.menu-item-has-children").removeClass("open").addClass("close");
+			$(".Nav ul.sub-menu").slideUp("fast").removeClass("open").addClass("close");
+		}
+	},50);
 });
 
-
-/* (モバイル版)
- * ナビゲーションメニュー第2階層
- * 開閉処理
+/* (PC版)
+ * ナビゲーションメニュー第1階層
+ * hover処理
 --------------------------------- */
-$(function(){
-    $(".Nav_btn a").click(function(){
-        if($(".Nav_body").css("display")=="none"){
-            $(".Nav_body").addClass("open").removeClass("close").slideDown("fast");
-            $(this).removeClass("close").addClass("open").html("<span>メニューを閉じる</span>");
-        }else{
-            $(".Nav_body").addClass("close").removeClass("open").slideUp("fast");
-            $(".Nav_body li.menu-item-has-children").removeClass("open").addClass("close");
-            $(".Nav ul.sub-menu").slideUp("fast").removeClass("open").addClass("close");
-            $(this).addClass("close").removeClass("open").html("<span>メニューを開く</span>");
-        }
-    });
+$(window).on('load',function(){
+	var w = window.innerWidth;
+    var point = 1200;
+	if(w <= point){
+		$("body").addClass("spn").removeClass("pc");
+		$(".Nav__btn a").addClass("close").removeClass("open").html("<span>メニューを開く</span>");
+	}else{
+		$("body").addClass("pc").removeClass("spn");
+		$(".Nav__btn a").removeClass("open close").html("<span>メニューを閉じる</span>");
+	}
 });
 
 /* (モバイル版)
- * ナビゲーションメニュー第2階層
- * 開閉時の「+」⇔「-」表示処理
+ * ナビゲーションメニュー第1階層
+ * 開閉処理(三 ⇔ ✕)
 --------------------------------- */
 $(function(){
-    $(".Nav_body li.menu-item-has-children").each(function(){
-        var submenu=$(this).find("ul.sub-menu");
-        var allsubmenu=$(".Nav ul.sub-menu");
-        var allbtn=$(".Nav_body li.menu-item-has-children");
-        $(this).addClass("close");
-        $(submenu).addClass("close");
-        $(this).hover(function(){
-            if($(this).hasClass("open")){
-                $(this).removeClass("open").addClass("close");
-                $(submenu).hide().removeClass("open").addClass("close");
-            }else{
-                $(allsubmenu).hide().removeClass("open").addClass("close");
-                $(allbtn).removeClass("open").addClass("close");$(submenu).slideDown("fast").removeClass("close").addClass("open");
-                $(this).removeClass("close").addClass("open");
-            }
-        });
-    });
+	$(".Nav__btn a").click(function(){
+		if($(".Nav__body").css("display")=="none"){
+			$(".Nav__body").addClass("open").removeClass("close").slideDown("fast");
+			$(this).removeClass("close").addClass("open").html("<span>メニューを閉じる</span>");
+		}else{
+			$(".Nav__body").addClass("close").removeClass("open").slideUp("fast");
+			$(".Nav__body li.menu-item-has-children").removeClass("open").addClass("close");
+			$(".Nav ul.sub-menu").slideUp("fast").removeClass("open").addClass("close");
+			$(this).addClass("close").removeClass("open").html("<span>メニューを開く</span>");
+		}
+	});
+});
+
+/* (モバイル版)
+ * ナビゲーションメニュー第2階層
+ * hover処理
+--------------------------------- */
+$(function(){
+	$(".Nav__body li.menu-item-has-children").each(function(){
+		var submenu=$(this).find("ul.sub-menu");
+		var allsubmenu=$(".Nav ul.sub-menu");
+		var allbtn=$(".Nav__body li.menu-item-has-children");
+		$(this).addClass("close");
+		$(submenu).addClass("close");
+		$(this).hover(function(){
+			if($(this).hasClass("open")){
+				$(this).removeClass("open").addClass("close");
+				$(submenu).hide().removeClass("open").addClass("close");
+			}else{
+				$(allsubmenu).hide().removeClass("open").addClass("close");
+				$(allbtn).removeClass("open").addClass("close");
+				$(submenu).slideDown("fast").removeClass("close").addClass("open");
+				$(this).removeClass("close").addClass("open");
+			}
+		});
+	});
 });
 
 /* (PC・モバイル版共通)
@@ -96,7 +91,18 @@ $(window).on('scroll', function(){
     var scrollTop = $(this).scrollTop(); /* スクロール量観測 */
     scrollTop = scrollTop / 5;           /* 除算する値が大きいほど移動量が小さくなる */
 
-    $('.ToHead_layer2').css(
+    $('.TopHead__layer2').css(
         'transform', 'translateY(' + scrollTop + 'px)'
     );
 });
+
+
+/* Headerにクラスの付与/解除(未使用)
+--------------------------------- */
+// $(window).on("scroll",function(){
+// 	var scroll=$(window).scrollTop();
+// 	if(scroll>0){$('.Header').addClass('is_fixed');
+// 	}else{
+// 		$('.Header').removeClass('is_fixed');
+// 	}
+// });
